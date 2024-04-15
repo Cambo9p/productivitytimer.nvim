78250eb73d37a8bbb46732e6e84e6b2f5bb60b56
cam <cambo9p.11@gmail.com>
2024-04-15 19:04:57 +1200

    chore: cleanup creating user command

--- plugin/productivitytimer.lua
+++ plugin/productivitytimer.lua
@@ -8,12 +8,22 @@ if vim.g.loaded_productivity_timer == 1 then
 end
 vim.g.loaded_productivity_timer = 1
 
-vim.api.nvim_set_hl(0, "CellularAutomatonNormal", { default = true, link = "Normal" })
+vim.api.nvim_create_user_command("StartTimer", function(args)
+    local input = args["line1"]
+    require("productivitytimer").StartTimer(input)
+end, {})
+if 1 ~= vim.fn.has "nvim-0.8.0" then
+    vim.apm.nvim_err_writeln("productivitytimer.nvim requires at least nvim-0.8.0")
+    return
+end
 
-vim.apm.create_user_command("StartTimer", function(opts)
-    require("productivity-timer").StartTimer(opts.fargs[1])
-end, {
-    nargs = 1,
-    complete = function (_, line)
-    end,
-})
+if vim.g.loaded_productivity_timer == 1 then
+  return
+end
+vim.g.loaded_productivity_timer = 1
+
+vim.api.nvim_create_user_command("StartTimer", function(args)
+    local input = args["line1"]
+    require("productivitytimer").StartTimer(input)
+end, {})
+
