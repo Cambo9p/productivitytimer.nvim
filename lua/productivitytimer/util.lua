1ac1613c505742a4a2af019bdafe7e19585cac28
cam <cambo9p.11@gmail.com>
2024-04-15 20:05:36 +1200

    chore: updated to work with ms due to vim api

--- lua/productivitytimer/util.lua
+++ lua/productivitytimer/util.lua
@@ -3,7 +3,11 @@ local utils = {}
 -- takes the input to the StartTimer function 
 --  and returns the time in seconds
 utils.parse_time = function (input)
+    print(input)
     local number, unit = string.match(input, "(%d+)(%a)")
+
+    print(number)
+    print(unit)
 
     if not number or not unit then
         return nil
@@ -14,10 +18,11 @@ utils.parse_time = function (input)
     if unit == "s" then
         return number
     elseif unit == "m" then
-        return number * 60
+        return number * 60 * 1000
     elseif unit == "h" then
-        return number * 3600
+        return number * 3600 * 1000
     end
+    print("no bueno")
 
     return nil
 end
