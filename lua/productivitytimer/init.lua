67687b522ff38b327c5ea9675f707e00e1ab3cde
cam <cambo9p.11@gmail.com>
2024-04-16 19:55:57 +1200

    chore: changed to use vim defer fn

--- lua/productivitytimer/init.lua
+++ lua/productivitytimer/init.lua
@@ -6,21 +6,15 @@ local function timer_callback()
     print("timer went off")
 end
 
-local timer = vim.loop.new_timer()
-
--- main entrypoint TODO: docs for useage
 M.StartTimer = function(time)
-    print("time is ")
-    print(time)
     local no_ms = util.parse_time(time)
     if not no_ms then
         print("There was an error parsing")
     end
-    timer:start(no_ms, 0, function()
+    vim.defer_fn(function()
         timer_callback()
-        timer:stop()
-    end)
-    print("got here")
+    end,
+    no_ms)
 end
 
 return M
